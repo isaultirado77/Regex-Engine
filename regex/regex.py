ed3a28a9b1f5c74b498b559a395da72ef93c208d
Isaul <tiradolisaul@gmail.com>
2024-11-18 20:54:51 -0700

    dot flag ready to implement

--- regex/regex.py
+++ regex/regex.py
@@ -1,5 +1,22 @@
+def read_input() -> str:
+    return input()
+
+
+def match(regex: str, string: str) -> bool:
+    if not regex:
+        return True
+    elif not string:
+        return True if not regex else False
+    elif '.' in regex:
+        return False
+    else:
+        return False
+
+
 def main():
-    pass
+    regex = read_input()
+    string = read_input()
+    print(match(regex, string))
 
 
 if __name__ == '__main__':
