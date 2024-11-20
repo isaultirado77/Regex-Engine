8c57d58cb41198fbecbd9be1ae839ba39b03da9f
Isaul <tiradolisaul@gmail.com>
2024-11-19 20:46:51 -0700

    search function: implementation in progress

--- regex/regex.py
+++ regex/regex.py
@@ -2,16 +2,22 @@ def read_input() -> str:
     return input()
 
 
-def match(regex: str, string: str):
+def match(regex: str, text: str):
     if not regex:
         return True
-    elif not string or regex[0] not in ('.', '', string[0]):
+    elif not text:
         return False
-    return match(regex[1:], string[1:])
+    elif not regex[0] == text[0] and regex[0] != '.':
+        return False
+    return match(regex[1:], text[1:])
 
 
-def find_flag(string: str, flag: str) -> list:
-    return list(filter(lambda i: string[i] == flag, range(len(string))))
+def search(regex: str, text: str):
+    pass
+
+
+def find_flag(text: str, flag: str) -> list:
+    return list(filter(lambda i: text[i] == flag, range(len(text))))
 
 
 def dot(regex: str, string: str) -> bool:
@@ -27,8 +33,8 @@ def dot(regex: str, string: str) -> bool:
 def main():
     usrinput = read_input()
     regex = usrinput.split('|')[0]
-    string = usrinput.split('|')[-1]
-    print(match(regex, string))
+    text = usrinput.split('|')[-1]
+    print(match(regex, text))
 
 
 if __name__ == '__main__':
